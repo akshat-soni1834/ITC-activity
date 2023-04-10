7facabd8279664f03b69e8fb894e09e537b2e7bf
akshat-soni1834 <126847522+akshat-soni1834@users.noreply.github.com>
2023-04-10 08:44:51 +0000

    main

--- Functions Exercise set/Problem_1.py
+++ Functions Exercise set/Problem_1.py
@@ -8,5 +8,7 @@ if prime.lower()=='yes':
         print('Order cost after 15% discount for prime memeber is',asset-discount_p)
         print('Order cost after 8% discount of black friday is',asset-black_fri)
         print('total payable amount after all discount is',asset-discount_p-black_fri)
+    else:
+        raise Typeerror('Enter a valid prime member id')
 elif prime.lower()=='no':
     print('Order cost after 8% discount of black friday is',asset-black_fri)
