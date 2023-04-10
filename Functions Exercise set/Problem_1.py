f02f48a030a8c2ccd9d9278f76040712f29a2942
akshat-soni1834 <126847522+akshat-soni1834@users.noreply.github.com>
2023-04-10 08:15:48 +0000

    main

--- Functions Exercise set/Problem_1.py
+++ Functions Exercise set/Problem_1.py
@@ -1,15 +1,12 @@
-#PROBLEM 1
 asset=int(input('Enter the total amount of your shopping :- '))
 black_fri=(asset*8)/100
 prime = input('Are you a prime member ? :-')
 if prime.lower()=='yes':
-prime_id=int(input('Enter your prime_id :- '))
-if (3456000<=prime_id<=3460000):
-discount_p=(asset*15)/100
-print('Order cost after 15% discount for prime memeber is',asset-
-discount_p)
-print('Order cost after 8% discount of black friday is',asset-black_fri)
-print('total payable amount after all discount is',asset-discount_p-
-black_fri)
+    prime_id=int(input('Enter your prime_id :- '))
+    if (3456000<=prime_id<=3460000):
+        discount_p=(asset*15)/100
+        print('Order cost after 15% discount for prime memeber is',asset-discount_p)
+        print('Order cost after 8% discount of black friday is',asset-black_fri)
+        print('total payable amount after all discount is',asset-discount_p-black_fri)
 elif prime.lower()=='no':
-print('Order cost after 8% discount of black friday is',asset-black_fri)
+    print('Order cost after 8% discount of black friday is',asset-black_fri)
